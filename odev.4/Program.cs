d0d8a511c7252336d965b00ef97e7f7732af4a95
tiyol <tiyol@LAPTOP-6IPT3PBJ>
2024-01-07 00:12:11 +0300

    Proje dosyası ekle.

---
+++ odev.4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int toplam = 0;
+        int sayi;
+
+        while (true)
+        {
+            Console.Write("Bir sayı girin: ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi == 83)
+            {
+                break;
+            }
+
+            toplam += sayi;
+        }
+
+        Console.WriteLine("Toplam = " + toplam);
+    }
+}
